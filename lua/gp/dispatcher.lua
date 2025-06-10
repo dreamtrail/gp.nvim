cb2c676fd3e6bace3bbe3d503defdb16a9cb4624
gituser <git@mailproper.com>
2025-06-10 10:32:33 +0900

    Fix: Handle thinking content correctly
    Make the decode for gemini more efficient.

--- lua/gp/dispatcher.lua
+++ lua/gp/dispatcher.lua
@@ -614,11 +614,8 @@ local query = function(buf, provider, payload, handler, on_exit, callback, strea
 
 				if qt.provider == "google" or qt.provider == "vertex" then
 					if line:match('"text":') then
-						pcall(function()
-							content = vim.json.decode("{" .. line .. "}").text
-						end)
 						-- If the content is thinking content, wrap it in <think> tags
-						if (type(content) ~= "string" or content == "") and line:sub(-1) == "," then
+						if payload.generationConfig.thinking_config and line:sub(-1) == "," then
 							pcall(function()
 								content = vim.json.decode("{" .. line:sub(1, -2) .. "}").text
 							end)
@@ -629,10 +626,15 @@ local query = function(buf, provider, payload, handler, on_exit, callback, strea
 								content = "<think>\n" .. content
 								total_reasoning_length = total_reasoning_length + #content
 							end
-						elseif total_reasoning_length > 0 and type(content) == "string" and content ~= "" then
-							content = content:gsub("^[\n]+", "")
-							content = "</think>\n\n" .. content
-							total_reasoning_length = -1
+						else
+							pcall(function()
+								content = vim.json.decode("{" .. line .. "}").text
+							end)
+							if total_reasoning_length > 0 then
+								content = content:gsub("^[\n]+", "")
+								content = "</think>\n\n" .. content
+								total_reasoning_length = -1
+							end
 						end
 					end
 				end
