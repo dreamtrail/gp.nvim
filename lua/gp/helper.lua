78bffd60d706806c474d3a4ab2b870aa26e01a26
gituser <git@mailproper.com>
2025-07-24 16:42:45 +0900

    Refactor delete_file to always remove files directly
    Comment out conditional trash logic and call os.remove unconditionally.
    This simplifies file deletion and avoids moving .md files to trash.

--- lua/gp/helper.lua
+++ lua/gp/helper.lua
@@ -194,11 +194,11 @@ _H.delete_file = function(file, callback)
 	end
 	_H.delete_buffer(file)
 	-- file file ends with .md then move to trash
-	if file:match("%.md$") then
-		_H.move_to_trash(file, callback)
-	else
-		os.remove(file)
-	end
+	-- if file:match("%.md$") then
+	-- 	_H.move_to_trash(file, callback)
+	-- else
+	os.remove(file)
+	-- end
 end
 
 ---@param file_name string # name of the file for which to get buffer
