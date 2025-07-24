f38ec36045eed586ee9f27b63f6a2091c4e39af2
gituser <git@mailproper.com>
2025-07-24 22:40:20 +0900

    Refactor: Improve file deletion error handling
    The `os.remove` function can fail, and the current implementation does not handle these failures gracefully. This commit updates the `delete_file` function to use `pcall` to catch potential errors during file deletion and log them appropriately. It also adds a notification for successful deletion and calls the provided callback function in both success and error scenarios.

--- lua/gp/helper.lua
+++ lua/gp/helper.lua
@@ -197,7 +197,34 @@ _H.delete_file = function(file, callback)
 	-- if file:match("%.md$") then
 	-- 	_H.move_to_trash(file, callback)
 	-- else
-	os.remove(file)
+	local success, err_or_nil = pcall(os.remove, file)
+	local file_name = file:match("([^/\\]+)$")
+	local msg = "Deleting file: " .. file_name
+	vim.fn.jobstart(cmd, {
+		on_exit = function()
+			vim.schedule(function()
+				-- vim.api.nvim_buf_delete(0, { force = true })
+				-- get the file name from the path and display it in the notification, need to compatible with windows and linux path
+				local file_name = file_path:match("([^/\\]+)$")
+				vim.notify("Moved to trash: " .. file_name, vim.log.levels.INFO)
+				if callback then
+					callback()
+				end
+			end)
+		end,
+	})
+	if success then
+		logger.debug("File deleted successfully: " .. file)
+		if callback then
+			callback()
+		end
+	else
+		if err_or_nil then
+			logger.error("Error deleting file: " .. file .. "\nError: " .. err_or_nil)
+		else
+			logger.error("Error deleting file: " .. file)
+		end
+	end
 	-- end
 end
 
